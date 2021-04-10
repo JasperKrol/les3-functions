c34f60c3474dc696e7b6725eccc746edd6e3fa76
JasperKrol <jasper.paul.krol@gmail.com>
2021-04-10 11:40:51 +0200

    oefeningen af functies en functies met parameters

--- functions-with-parameters.js
+++ functions-with-parameters.js
@@ -17,7 +17,12 @@ console.log(result);
 // greeter("Jan") geeft "Hoi Jan!"
 // greeter("Kees") geeft "Hoi Kees!"
 
+function greeter (name) {
+  return "hi " + name + "!";
+}
 
+const greetings = greeter("jan");
+console.log(greetings)
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd minutesToSeconds, die een hoeveelheid minuten verwacht (als een getal) en teruggeeft hoeveel seconden dat zijn.
@@ -26,7 +31,12 @@ console.log(result);
 // minutesToSeconds(3) geeft 180
 // minutesToSeconds(23) geeft 1380
 
+function minutesToSeconds(minutes) {
+  return minutes * 60;
+}
 
+const calculateMinutes = minutesToSeconds(1);
+console.log(calculateMinutes)
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd merge, die twee strings verwacht en deze aan elkaar geplakt teruggeeft.
@@ -35,8 +45,12 @@ console.log(result);
 // merge("abra", "cadabra") geeft "abracadabra"
 // merge("zoet", "sappig") geeft "zoetsappig"
 
+function merge (word1, word2) {
+  return  word1+word2;
+}
 
-
+const merging = merge("abra", "kadabra");
+console.log(merging)
 
 /* Opdracht  4 */
 // Schrijf een functie genaamd calculateDogYears die de hondenleeftijd verwacht en dit omrekent naar mensenjaren (1 tot 7). Op basis daarvan wordt een zinnetje teruggegeven.
@@ -44,7 +58,13 @@ console.log(result);
 // calculateDogYears(6) geeft "Jouw hond is 42 jaar oud in mensenjaren."
 // calculateDogYears(2) geeft "Jouw hond is 14 jaar oud in mensenjaren."
 
+function calculateDogYears (dogAge) {
+  const humanAge = dogAge * 7;
+  return "Jouw hond is " + humanAge + " jaar oud in mensenjaren."
+}
 
+const calculateAge = calculateDogYears(6);
+console.log(calculateAge)
 
 /* Opdracht 5 */
 // Schrijf een functie genaamd wrapper, die twee parameters verwacht: een woord en een karakter
@@ -54,6 +74,12 @@ console.log(result);
 // wrapper("beep", "_") geeft "_beep_"
 // wrapper("kaas", "Q") geeft "QkaasQ"
 
+function wrapper (word, character) {
+  return character+word+character;
+}
+
+const toWrap = wrapper("beep","_");
+console.log(toWrap)
 
 
 /* Bonus opdracht  */
@@ -62,4 +88,9 @@ console.log(result);
 // createDetailString({ firstName: 'Jan', lastName: 'Jansen', profession: 'docent'}) geeft "Het beroep vam Jan Jansen is docent."
 // createDetailString({ firstName: 'Kees', lastName: 'Klaasen', profession: 'brandweerman'}) geeft "Het beroep vam Kees Klaasen is brandweerman."
 
+function createDetailString (object) {
+  return "Het beroep van " + object.firstName + " " + object.lastName + " is " + object.profession;
+}
 
+const detailString = createDetailString({ firstName: 'Jan', lastName: 'Jansen', profession: 'docent'})
+console.log(detailString);
--- functions.js
+++ functions.js
@@ -14,20 +14,41 @@ console.log(greeting);
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
 
+function getSchool () {
+    return "Novi";
+}
 
+const atSchool = getSchool()
+console.log(atSchool)
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
 
+function compliment (name){
+    return "lekker bezig " + name;
+}
 
-
+const giveCompliment = compliment("jas")
+console.log(giveCompliment)
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
+function getGrades (grades){
+    return grades;
+}
 
+const gottenGrades = getGrades([8, 8.5, 6, 7])
+console.log(gottenGrades)
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+function getDetails (inforObject){
+    return inforObject
+}
+
+const givenDetails = getDetails({firstName: "Jan", secondName: "Peters"})
+console.log(givenDetails)
